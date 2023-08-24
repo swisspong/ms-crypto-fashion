4ae9c569481ebb7b775a3884582d3cb033605ded
swisspong <swisspong@gmail.com>
2023-08-24 19:05:03 +0700

    add microservice

--- ms-crypto-fashion/apps/products/src/main.ts
+++ ms-crypto-fashion/apps/products/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ProductsModule } from './products.module';
+import { RmqService } from '@app/common/rmq/rmq.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProductsModule);
+  const rmqService = app.get<RmqService>(RmqService)
+  app.connectMicroservice(rmqService.getOptoins('PRODUCTS'))
+  await app.startAllMicroservices()
   await app.listen(3000);
 }
 bootstrap();
--- ms-crypto-fashion/apps/products/src/products.module.ts
+++ ms-crypto-fashion/apps/products/src/products.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { RmqModule } from '@app/common/rmq/rmq.module';
+import { PRODUCTS_SERVICE } from './constants/services';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+        // PORT: Joi.number().required(),
+      })
+    }),
+    RmqModule.register({ name: PRODUCTS_SERVICE })
+  ],
+
   controllers: [ProductsController],
   providers: [ProductsService],
 })
-export class ProductsModule {}
+export class ProductsModule { }
