9fd4766159126a8f3b980c3a779e800a98bde5d6
swisspong <swisspong@gmail.com>
2023-09-08 18:54:08 +0700

    check item on ordering

--- ms-crypto-fashion/apps/carts/src/checkouts/checkouts.controller.ts
+++ ms-crypto-fashion/apps/carts/src/checkouts/checkouts.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { CheckoutsService } from './checkouts.service';
+import { CreateCheckoutItemsDto } from '../dto/create-checkout.dto';
+import { GetUserId } from '@app/common/decorators';
 
 @Controller('checkouts')
 export class CheckoutsController {
-  constructor(private readonly checkoutsService: CheckoutsService) {}
+  constructor(private readonly checkoutsService: CheckoutsService) { }
+  @Post()
+  checkout(@GetUserId() userId: string, createCheckoutDto: CreateCheckoutItemsDto) {
+    return this.checkoutsService.createCheckoutItems(userId, createCheckoutDto)
+  }
+  @Get('/:id')
+  getCheckout(@Param(':id') id: string) {
+    return this.checkoutsService.getCheckout(id)
+  }
+
+  @Post('/:id')
+  ordering(@Param('id') id: string) {
+    return this.checkoutsService
+  }
 }
