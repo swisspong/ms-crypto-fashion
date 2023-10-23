3b932c1bc19ff7c9f0e093302db9dbc996d3dc72
swisspong <swisspong@gmail.com>
2023-10-24 02:45:50 +0700

    add real domain

--- customer-frontend/components/google-button.tsx
+++ customer-frontend/components/google-button.tsx
@@ -7,7 +7,7 @@ const GoogleButton = () => {
   return (
     <Link
     //   className="w-full"
-      href={"http://api.example.com/auth/google"}
+      href={"http://api.cryptofashion.store/auth/google"}
 
         // className="mb-3 flex w-full items-center justify-center rounded bg-primary px-7 pb-2.5 pt-3 text-center text-sm font-medium  leading-normal  shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
       //   style={{ backgroundColor: "#ffffff" }}
