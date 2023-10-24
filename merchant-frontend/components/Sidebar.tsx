8f9ac601f20fa46da98087d0b9971710a09f27fd
swisspong <swisspong@gmail.com>
2023-10-24 20:08:28 +0700

    fix sidebar heading

--- merchant-frontend/components/Sidebar.tsx
+++ merchant-frontend/components/Sidebar.tsx
@@ -63,9 +63,9 @@ export function Sidebar({ className }: SidebarProps) {
     <div className={cn("pb-12", className)}>
       <div className="space-y-4 py-4">
         <div className="px-0 text-center md:px-4  py-2">
-          <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
+          {/* <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
             Merchant Crypto Fashion
-          </h2>
+          </h2> */}
           <div className="space-y-1">
             {sidebarItems.map(({ icon: Icon, name, href }) => {
               return (
