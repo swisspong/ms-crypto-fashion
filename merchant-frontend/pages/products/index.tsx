09ce6f07b9429e2555d6bdccb9a7ee4aca432b42
swisspong <swisspong@gmail.com>
2023-10-24 20:17:26 +0700

    fix justify

--- merchant-frontend/pages/products/index.tsx
+++ merchant-frontend/pages/products/index.tsx
@@ -167,7 +167,7 @@ export default function Role() {
           </div>
         ))}
         <div className="px-2 mb-1 block md:hidden ">
-          <div className="flex items-center space-x-3 w-full">
+          <div className="flex items-center justify-center space-x-3 w-full">
             <p>ไม่มีข้อมูล</p>
           </div>
         </div>
