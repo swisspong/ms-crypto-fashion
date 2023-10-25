efc75e1093ee83f9b5ce21e5875baa9d6aaf0cb2
swisspong <swisspong@gmail.com>
2023-10-26 05:52:12 +0700

    admin next

--- admin-frontend/lib/utils.ts
+++ admin-frontend/lib/utils.ts
@@ -44,7 +44,7 @@ interface DynamicApiOptions {
 const urlFactory = (value: SERVICE_FORMAT) => {
   // const csrString = 'http://api.example.com'
   // const csrString = 'http://api.cryptofashion.store'
-  const csrString = `${process.env.HOST_API}`
+  const csrString = `${process.env.NEXT_PUBLIC_HOST_API}`
   const ssrString = 'http://'
   switch (value) {
     case SERVICE_FORMAT.AUTH:
